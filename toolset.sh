58c950ac40b8907cf4542eddb2246dcc2f6b2a78
Guillermo Cura <guillermocura28@gmail.com>
2019-11-01 06:20:40 -0500

    Update toolset.sh

--- toolset.sh
+++ toolset.sh
@@ -3,6 +3,13 @@ mkdir /opt/tools
 echo "Making wordlists directory in /opt/wordlists"
 mkdir /opt/wordlists
 
+echo "Grabbing Responder/MultiRelay"
+git clone https://github.com/lgandx/Responder /opt/tools
+echo "Responder/MultiRelay will be in /opt/tools directory"
+
+
 echo "Grabbing SecLists"
 git clone https://github.com/danielmiessler/SecLists.git /opt/wordlists
-echo "SecList will be in /usr/share/ directory"
+echo "SecList will be in /opt/wordlists directory"
+
+
