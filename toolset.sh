3f89c9ae67c8310268d46600a4f71baf4fbe59cf
Guillermo Cura <guillermocura28@gmail.com>
2019-11-01 05:43:55 -0500

    Update toolset.sh

--- toolset.sh
+++ toolset.sh
@@ -1,3 +1,8 @@
+echo "Making tools directory in /opt/tools"
+mkdir /opt/tools
+echo "Making wordlists directory in /opt/wordlists"
+mkdir /opt/wordlists
+
 echo "Grabbing SecLists"
-git -C /usr/share clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/danielmiessler/SecLists.git /opt/wordlists
 echo "SecList will be in /usr/share/ directory"
